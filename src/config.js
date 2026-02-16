e851aaad21fbd8a828870dbf0e7e808d87df2f87
EfVictor <efimov840@gmail.com>
2026-02-16 12:47:31 +0300

    Update gitignore

--- src/config.js
+++
@@ -1,6 +0,0 @@
-// Paths to Node.js server commands
-const WriteJSON_URL = 'http://localhost:4998/WRITE'
-const SendData_URL = 'http://localhost:4998/SEND'
-const UpdateData_URL = 'http://localhost:4998/UPDATE'
-const StartService_URL = 'http://localhost:4998/START'
-const StopService_URL = 'http://localhost:4998/STOP'
--- src/launch_node_tablo.bat
+++
@@ -1,2 +0,0 @@
-node.exe "node_tablo.js"
-pause
